0757e8df6a6f7e3a4c7a7766a9bdfa9202b15241
okean8901 <trangialoc892001@gmail.com>
2025-03-20 16:20:14 +0700

    update repositories

---
+++ StudentManagementSystem/Repositories/CourseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Data;
+using StudentManagementSystem.Models.Entities;
+
+namespace StudentManagementSystem.Repositories
+{
+    public class CourseRepository : IRepository<Course>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CourseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Course> GetByIdAsync(int id)
+        {
+            return await _context.Courses.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Course>> GetAllAsync()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        public async Task AddAsync(Course entity)
+        {
+            await _context.Courses.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Course entity)
+        {
+            _context.Courses.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _context.Courses.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
